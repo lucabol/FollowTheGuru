70d24f3af7f3a29ce009dca62f83939d0b40fc04
Luca Bolognese <lucabol@microsoft.com>
2016-11-09 16:12:39 +0100

    Added comments for main method

--- ConsoleFollower/Program.cs
+++ ConsoleFollower/Program.cs
@@ -13,15 +13,17 @@ public class Program {
     static string DisplayPortToString(DisplayPortfolio dp) {
         var sb = new StringBuilder();
         sb.AppendLine(String.Join(" -- ", dp.DisplayName,dp.EndQuarterDate.ToString("d"), dp.TotalValue, dp.PositionsNumber));
-        sb.AppendLine(String.Join(" ", "NEW ", "SOLD", cs("NAME", 30), cs("P/C", 5), cs("SHARES", 10),
+        sb.AppendLine(String.Join(" ", "NEW ", "SOLD", cs("NAME", 40), cs("P/C", 5), cs("SHARES", 10),
                                     cs("VALUE", 10), "%PORT", "CHANGE"));
         foreach (var p in dp.Positions) {
-            sb.AppendLine(String.Join(" ", def(p.IsNew, "NEW "), def(p.IsSold,"SOLD"), cs(p.Name.Trim(),30), cs(p.PutCall,5) , cs(p.Shares.ToString(),10),
+            sb.AppendLine(String.Join(" ", def(p.IsNew, "NEW "), def(p.IsSold,"SOLD"), cs(p.Name.Trim(),40), cs(p.PutCall,5) , cs(p.Shares.ToString(),10),
                                     cs(p.Value.ToString(),10), Math.Round(p.PercOfPortfolio * 100,2), Math.Round(p.Change * 100,2)));
         }
         return sb.ToString();
     }
 
+    // Try with the following ciks: 0001553733, 0001568820, 0001484148, 0001112520
+    // or go to https://www.sec.gov/edgar/searchedgar/companysearch.html and put the name of the investor you are interested in
     public static void Main(string[] args)
     {
         if (args.Count() != 1) throw new Exception("Usage: Follower Cik");
